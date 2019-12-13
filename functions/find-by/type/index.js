a082bcad78ce8db3b6902b20537b776b09c7e0b3
Mohanad-Abdalradi <ar_mohanad@hotmail.com>
2019-12-13 22:22:16 +0100

    find-by/type

--- functions/find-by/type/index.js
+++ functions/find-by/type/index.js
@@ -1,5 +1,7 @@
 const type = (pokeArray, type) => {
-
+  let results = pokeArray.filter(pokemon => pokemon.type.includes(type));
+  results = results.map(pokemon => ({ name: pokemon.name, num: pokemon.num }));
+  return results;
 };
 
 module.exports = type;
