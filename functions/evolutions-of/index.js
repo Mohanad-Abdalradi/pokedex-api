1408548c2c8fc632a16ca8d27b6e6316f743464c
Mohanad-Abdalradi <ar_mohanad@hotmail.com>
2019-12-13 22:13:42 +0100

    evolutions-of

--- functions/evolutions-of/index.js
+++ functions/evolutions-of/index.js
@@ -1,5 +1,21 @@
 const evolutionOf = (pokeArray, name) => {
+  const pokemon = pokeArray.find(pokemon => pokemon.name === name);
 
+  if (!pokemon) return [];
+
+  const allEvolutions = [];
+
+  if (Array.isArray(pokemon.prev_evolution)) {
+    allEvolutions.push(...pokemon.prev_evolution);
+  }
+
+  allEvolutions.push({ num: pokemon.num, name: pokemon.name });
+
+  if (Array.isArray(pokemon.next_evolution)) {
+    allEvolutions.push(...pokemon.next_evolution);
+  }
+
+  return allEvolutions;
 };
 
 module.exports = evolutionOf;
